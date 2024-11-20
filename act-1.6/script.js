5e7b92ce09eb81f490fe588f4a1e5ab905b27573
alethea03 <89721998+alethea03@users.noreply.github.com>
2024-11-20 22:32:23 +0800

    Update script.js

--- act-1.6/script.js
+++ act-1.6/script.js
@@ -1,56 +1,75 @@
-import * as THREE from 'three' 
+import * as THREE from "https://esm.sh/three"
+import { OrbitControls } from "https://esm.sh/three/examples/jsm/controls/OrbitControls.js";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerwidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 2;
+const canvas = document.querySelector('canvas.webgl')
 
-const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('.webgl')});
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+const scene = new THREE.Scene()
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({color: 0x8F00FF})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+const camera = new THREE.PerspectiveCamera(75,sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+window.addEventListener('resize', () =>{
+  
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+})
+
+window.addEventListener('dblclick', () => {
+    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+    if (!fullscreenElement){
+        if(canvas.requestFullscreen){
+            canvas.requestFullscreen()
+        }else if (canvas.webkitRequestFullscreen){
+            canvas.webkitRequestFullscreen()
+        }
+    }else {
+        if(document.exitFullscreen){
+            document.exitFullscreen()
+        }else if(document.webkitExitFullscreen){
+            document.webkitExitFullscreen()
+        }
+    }
+})
+
+// Animation Loop
 const animate = () => {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // Update controls
+    controls.update();
 
-
+    // Render the scene
     renderer.render(scene, camera);
+
+    // Request next frame
     requestAnimationFrame(animate);
 };
 
 animate();
-
-window.addEventListener('resize', () => {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-
-camera.aspect = width / height;
-camera.updateProjectionMatrix();
-
-renderer.setSize(width, height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-});
-
-window.addEventListener('dblclick', () => {
-    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement;
-    const canvas = renderer.domElement;
-
-    if(!fullscreenElement){
-        if(canvas.requestFullscreen){
-            canvas.requestFullscreen();
-        } else if (canvas.webkitFullscreenElement){
-            canvas.webkitRequestFullscreen();
-        }
-    } else{
-        if(document.exitFullscreen){
-            document.exitFullscreen();
-        } else if(document.webkitFullscreenElement){
-            document.webkitExitFullscreen();
-    }
-}
-});
